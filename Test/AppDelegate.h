2de51fd5f938fbd4b304e8b7a566e3dc2fd203f7
Linraine <linraine@LinrainematoMacBook-Pro.local>
2014-05-20 15:42:48 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Linraine on 5/20/14.
+//  Copyright (c) 2014 Linraine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
